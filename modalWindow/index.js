b6bd92bd8894bee9c493d59f6eff8767090ece18
9vsegda1 <makki.nem@yandex.ru>
2020-03-01 17:40:40 +0700

    Ready for lesson 2

--- modalWindow/index.js
+++ modalWindow/index.js
@@ -1,2 +1,36 @@
-const modal = $.modal()
+const fruits = [
+  {id: 1, title: 'Яблоки', price: 20, img: ''}
+  {id: 2, title: 'Апельсины', price: 30, img: ''}
+  {id: 3, title: 'Манго', price: 40, img: ''}
+]
+
+
+/*
+* 1. Динамически на основе массива вывести список карточек
+* 2. Показать цену в модалке (это должна быть одна модалка)
+* 3. Модалка для удаления с 2мя кнопками (удалить отмена)
+* --------------------
+* 4. При удалении убрать карточку из дом дерева
+* {на основе $.modal сделать $.confirm (Promise)}
+*
+*
+* */
+
+const modal = $.modal({
+  title: 'Test title',
+  closable: true,
+  content: `
+    <p>Lorem ipsum dolor sit.</p>
+    <p>Lorem ipsum dolor sit.</p>
+  `,
+  width: '400px',
+  footerButtons: [
+    {text: 'Ok', type: 'primary', handler() {
+      console.log('Primary btn clicked')
+    }},
+    {text: 'Cansel', type: 'danger', handler() {
+      console.log('Danger btn clicked')
+    }}
+  ]
+})
 // myModal.open()
