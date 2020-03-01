6328de822e8371f43b44a47e877a2d42c5ec4c1d
9vsegda1 <makki.nem@yandex.ru>
2020-03-01 19:08:36 +0700

    Dynamic cards spawning

--- modalWindow/index.js
+++ modalWindow/index.js
@@ -1,12 +1,41 @@
 const fruits = [
-  {id: 1, title: 'Яблоки', price: 20, img: ''},
-  {id: 2, title: 'Апельсины', price: 30, img: ''},
-  {id: 3, title: 'Манго', price: 40, img: ''},
+  {id: 1, title: 'Яблоки', price: 20, img: 'https://xn----7sbaoeae1bm2adnh7g.xn--p1ai/uploads/s/u/i/3/ui3nkmtbnsta/img/full_oDCjxV8h.jpg'},
+  {id: 2, title: 'Апельсины', price: 30, img: 'https://avatars.mds.yandex.net/get-zen_doc/1590365/pub_5e29ce6b78125e00b13d3f0a_5e29cfc22b616959fd64242c/scale_1200'},
+  {id: 3, title: 'Манго', price: 40, img: 'https://i2.wp.com/journey-assist.com/wp-content/uploads/2019/08/%D0%9C%D0%B0%D0%BD%D0%B3%D0%BE.jpg?fit=1200%2C960&ssl=1'},
 ]
 
 
+
+function cardsGenerate(cards){
+  const wrap = document.createElement('div')
+  wrap.classList.add('row')
+
+  cards.forEach(elem => {
+
+    const card = document.createElement('div')
+    card.classList.add('col')
+    card.insertAdjacentHTML('afterbegin', `
+      <div class="card">
+        <img class="card-img-top" style="height: 300px" src="${elem.img}" alt="Card image">
+        <div class="card-body">
+          <h5 class="card-title">${elem.title}</h5>
+          <a href="#" class="btn btn-primary">Посмотреть цену</a>
+          <a href="#" class="btn btn-danger">Удалить</a>
+        </div>
+      </div>
+    `)
+
+    wrap.appendChild(card)
+  })
+
+  return wrap
+}
+
+document.querySelector('.container').append(cardsGenerate(fruits))
+
+
 /*
-* 1. Динамически на основе массива вывести список карточек
+* 1. Динамически на основе массива вывести список карточек +
 * 2. Показать цену в модалке (это должна быть одна модалка)
 * 3. Модалка для удаления с 2мя кнопками (удалить отмена)
 * --------------------
@@ -15,6 +44,12 @@ const fruits = [
 *
 *
 * */
+
+
+
+
+
+
 
 const modal = $.modal({
   title: 'Test title',
@@ -33,5 +68,15 @@ const modal = $.modal({
     }}
   ]
 })
+
+
+
+
+
+
+
+
+
+
 // myModal.open()
 modal.onClose(() => console.log('on close hook'))
